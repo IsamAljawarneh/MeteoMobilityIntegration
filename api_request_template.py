cdf7ac3764622237d7b7e4bf913cb10c0b89d710
Denis Pereira <denis.pereira@workday.com>
2021-04-21 22:48:09 +0100

    First addition to the repo

---
+++ api_request_template.py
@@ -0,0 +1,31 @@
+import cdsapi
+import sys
+"""
+Usage:
+$ python3 api_request.py <variable> <start_date> <end_date> <min_lon> <max_lon> <min_lat> <max_lat> <downoad_file_name>
+"""
+
+variable, start_date, end_date, min_lon, max_lon, min_lat, max_lat, downoad_file_name = sys.argv
+
+def main(argv):
+
+    c = cdsapi.Client()
+
+    c.retrieve(
+        'cams-global-reanalysis-eac4',
+        {
+            'date':  "{}/{}".format(start_date, end_date),
+            'format': 'grib',
+            'variable': variable,
+            'time': [
+                '12:00', '15:00',
+            ],
+            'area': [
+                int(min_lon), int(min_lat), int(max_lon),
+                int(max_lat),
+            ],
+        },
+         download_file_name)
+
+if __name__ == "__main__":
+   main(sys.argv)
---
+++ grib2csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys, getopt
+
+def main(argv):
+
+   input_file = ''
+   output_file = ''
+
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+
+   except getopt.GetoptError:
+      print('grib2csv.py -i <input_file> -o <output_file>')
+      sys.exit(2)
+
+   for opt, arg in opts:
+      if opt == '-h':
+         print('grib2csv.py -i <input_file> -o <output_file>')
+         sys.exit()
+      elif opt in ("-i", "--input"):
+         input_file = arg
+      elif opt in ("-o", "--output"):
+         output_file = arg
+   print('Input file is ', input_file)
+   print('Output file is ', output_file)
+
+   with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
+      [f_out.write(','.join(line.split()) + '\n') for line in f_in]
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
